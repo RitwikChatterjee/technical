df418831f379518958b73e73a93353203c47ad7f
ritwikchatterjee <ritwik.chatterjee@gmail.com>
2018-08-23 17:11:04 -0700

    New Subject - Accounting
    Initial add

---
+++ Accounting/Notes/Accounting.mm
@@ -0,0 +1,112 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Accounting" FOLDED="false" ID="ID_1263864849" CREATED="1533793383990" MODIFIED="1533793390044" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Underlying rules" POSITION="right" ID="ID_1787467684" CREATED="1533793392692" MODIFIED="1533793419520">
+<edge COLOR="#ff0000"/>
+<node TEXT="Shortcut" ID="ID_1862386632" CREATED="1533793421438" MODIFIED="1533793425723">
+<node TEXT="DC ADE LER" ID="ID_172083267" CREATED="1533793766464" MODIFIED="1533793781511">
+<hook URI="Accounting_files/DC%20ADE%20LER.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Debits = Credits" ID="ID_1987167597" CREATED="1533793879846" MODIFIED="1533793886434"/>
+<node TEXT="Assets = Liability + Equity" ID="ID_1718180607" CREATED="1533794501982" MODIFIED="1533794513196"/>
+</node>
+<node TEXT="Examples" POSITION="right" ID="ID_1790468322" CREATED="1533794105644" MODIFIED="1533794118076">
+<edge COLOR="#0000ff"/>
+<node TEXT="Assets" FOLDED="true" ID="ID_293849746" CREATED="1533794119543" MODIFIED="1533794122177">
+<node TEXT="Cash" ID="ID_254104309" CREATED="1533794123712" MODIFIED="1533794127198"/>
+<node TEXT="Accounts receivable" ID="ID_942235199" CREATED="1533794128068" MODIFIED="1533794134888"/>
+<node TEXT="Inventory" ID="ID_1237083620" CREATED="1533794135345" MODIFIED="1533794137678"/>
+<node TEXT="Equipment" ID="ID_133307171" CREATED="1533794138520" MODIFIED="1533794146974"/>
+<node TEXT="Building" ID="ID_1911045904" CREATED="1533794147827" MODIFIED="1533794149640"/>
+</node>
+<node TEXT="Draw" FOLDED="true" ID="ID_1298279527" CREATED="1533794224101" MODIFIED="1533794228136">
+<node TEXT="Withdrawal" ID="ID_1178609491" CREATED="1533794233713" MODIFIED="1533794271852"/>
+<node TEXT="Distribution" ID="ID_508998526" CREATED="1533794250712" MODIFIED="1533794254896"/>
+</node>
+<node TEXT="Expenses" FOLDED="true" ID="ID_1232263448" CREATED="1533794278242" MODIFIED="1533794282119">
+<node TEXT="Rent" ID="ID_1503346781" CREATED="1533794302179" MODIFIED="1533794306409"/>
+<node TEXT="Utilities" ID="ID_1299712117" CREATED="1533794307291" MODIFIED="1533794309111"/>
+<node TEXT="Telephone" ID="ID_185121748" CREATED="1533794309729" MODIFIED="1533794312342"/>
+<node TEXT="Internet" ID="ID_1073237841" CREATED="1533794312727" MODIFIED="1533794315056"/>
+</node>
+<node TEXT="Liabilities" FOLDED="true" ID="ID_1252573094" CREATED="1533794329220" MODIFIED="1533794332755">
+<node TEXT="A/cs payable" ID="ID_1784439086" CREATED="1533794333912" MODIFIED="1533794337992"/>
+<node TEXT="Wages payable" ID="ID_279159092" CREATED="1533794339122" MODIFIED="1533794345740"/>
+</node>
+<node TEXT="Equity (Owner&apos;s)" FOLDED="true" ID="ID_1815432422" CREATED="1533794348312" MODIFIED="1533794360774">
+<node TEXT="Retained earnings" ID="ID_242987454" CREATED="1533794361921" MODIFIED="1533794366414"/>
+</node>
+<node TEXT="Revenue" ID="ID_170313272" CREATED="1533794370624" MODIFIED="1533794379076"/>
+</node>
+<node TEXT="Definitions" FOLDED="true" POSITION="left" ID="ID_398127675" CREATED="1533794431716" MODIFIED="1533794434565">
+<edge COLOR="#00ff00"/>
+<node TEXT="Income Statement" FOLDED="true" ID="ID_728030639" CREATED="1533794436614" MODIFIED="1533794439731">
+<node TEXT="Expenses + Revenue" ID="ID_256276752" CREATED="1533794441993" MODIFIED="1533794449095"/>
+</node>
+</node>
+</node>
+</map>
