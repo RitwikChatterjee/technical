1ba214096fffdfbc7022c79756ead05a39cf2c6f
Ritwik Chatterjee <ritwik@gmail.com>
2018-08-30 13:03:38 -0700

    Added Security

---
+++ Security/Webinar/Identity Federation Patterns (WSO2).mm
@@ -0,0 +1,390 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Identity Federation Patterns (WSO2)" FOLDED="false" ID="ID_524456263" CREATED="1500392066160" MODIFIED="1500393889959" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="About" POSITION="left" ID="ID_818545536" CREATED="1500393895124" MODIFIED="1500393899821">
+<edge COLOR="#00ff00"/>
+<node TEXT="WSO2 Webinar" ID="ID_1193440886" CREATED="1500393914373" MODIFIED="1500393920662">
+<node TEXT="July 18, 2017" ID="ID_47192519" CREATED="1500393922387" MODIFIED="1500393940190"/>
+</node>
+</node>
+<node TEXT="WSO2 Platform" FOLDED="true" POSITION="left" ID="ID_963462010" CREATED="1500393908595" MODIFIED="1500394050243">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Platform" FOLDED="true" ID="ID_756650966" CREATED="1500394063080" MODIFIED="1500394071320">
+<node TEXT="Identity Federation Patterns (WSO2)_918292108906217667.png" ID="ID_1065732490" CREATED="1500394085116" MODIFIED="1500394159585" TEXT_SHORTENED="true">
+<hook URI="Identity%20Federation%20Patterns%20(WSO2)_918292108906217667.png" SIZE="0.62305295" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Identity Server" FOLDED="true" ID="ID_1228942283" CREATED="1500394143137" MODIFIED="1500394150643">
+<node TEXT="Industry Standards" ID="ID_1285185956" CREATED="1500394380537" MODIFIED="1500394385386"/>
+<node TEXT="Built-in connectors" ID="ID_1208942391" CREATED="1500394386616" MODIFIED="1500394392194"/>
+</node>
+</node>
+<node TEXT="Need" FOLDED="true" POSITION="right" ID="ID_394348921" CREATED="1500394187738" MODIFIED="1500394761015">
+<edge COLOR="#00ffff"/>
+<node TEXT="Need for Identity as web evolves" ID="ID_1971524125" CREATED="1500394234155" MODIFIED="1500394802762">
+<node TEXT="Needs Identity of user (Web 2.0)" ID="ID_1908045428" CREATED="1500394306285" MODIFIED="1500394321319"/>
+<node TEXT="Needs identity of things (Web 3.0)" ID="ID_619722305" CREATED="1500394324109" MODIFIED="1500394333640"/>
+</node>
+<node TEXT="IAM Requirements" ID="ID_1212354018" CREATED="1500394558381" MODIFIED="1500394564553">
+<node TEXT="In general" ID="ID_1554934245" CREATED="1500394566086" MODIFIED="1500394588245">
+<node TEXT="Seamless UX" ID="ID_1080018695" CREATED="1500394591393" MODIFIED="1500394603370"/>
+</node>
+<node TEXT="Consumer" ID="ID_1453820339" CREATED="1500394604959" MODIFIED="1500394608850">
+<node TEXT="Easy access" ID="ID_1652521920" CREATED="1500394613377" MODIFIED="1500394707982"/>
+</node>
+<node TEXT="Enterprise" ID="ID_382414018" CREATED="1500394628161" MODIFIED="1500394631388">
+<node TEXT="New business demands" ID="ID_1001315521" CREATED="1500394720836" MODIFIED="1500394725903"/>
+<node TEXT="Complex policies" ID="ID_270644672" CREATED="1500394632810" MODIFIED="1500394642324">
+<node TEXT="Pwd policies" ID="ID_1772145207" CREATED="1500394643842" MODIFIED="1500394648884"/>
+<node TEXT="Strong auth" ID="ID_1256915966" CREATED="1500394649553" MODIFIED="1500394654972"/>
+<node TEXT="Login policies" ID="ID_778872623" CREATED="1500394657218" MODIFIED="1500394660813"/>
+<node TEXT="Regulations" ID="ID_1466472564" CREATED="1500394664530" MODIFIED="1500394667517"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Identity Federation" POSITION="right" ID="ID_1994019267" CREATED="1500394201281" MODIFIED="1500394925923">
+<edge COLOR="#7c0000"/>
+<node TEXT="What?" ID="ID_1219367155" CREATED="1500394868199" MODIFIED="1500394872706">
+<node TEXT="Connecting person&apos;s digital identity and attributes" ID="ID_1435066078" CREATED="1500394874159" MODIFIED="1500394892964"/>
+<node TEXT="stored across multiple distinct trust domains" ID="ID_1962273776" CREATED="1500394893863" MODIFIED="1500394905714"/>
+</node>
+<node TEXT="Benefits" FOLDED="true" ID="ID_609118597" CREATED="1500394927776" MODIFIED="1502998888562">
+<node TEXT="Elevated security" ID="ID_1171053532" CREATED="1500394933954" MODIFIED="1500394943259">
+<node TEXT="Actual identity in one trust domain" ID="ID_680259808" CREATED="1500394944593" MODIFIED="1500394955219"/>
+<node TEXT="Other trust domains connect to get your identity" ID="ID_121806786" CREATED="1500394957569" MODIFIED="1500394967988"/>
+<node TEXT="Reduced surface for attack" ID="ID_368801770" CREATED="1500395003706" MODIFIED="1500395112783"/>
+<node TEXT="Can introduce MFA" ID="ID_1772694531" CREATED="1500395080164" MODIFIED="1500395087407"/>
+</node>
+<node TEXT="Standards" ID="ID_1834550594" CREATED="1500394975754" MODIFIED="1502998888562">
+<node TEXT="Leverages industry standards (SAML, OpenID, OAuth)" ID="ID_1483661080" CREATED="1500394982995" MODIFIED="1500395333917"/>
+</node>
+<node TEXT="Easier to maintain" ID="ID_1683945923" CREATED="1500395053490" MODIFIED="1500395128712">
+<node TEXT="User has only one set of credentials to remember" ID="ID_529108050" CREATED="1500394988465" MODIFIED="1500395002748"/>
+</node>
+<node TEXT="Enables SSO" ID="ID_189938974" CREATED="1500395068092" MODIFIED="1500395075850"/>
+<node TEXT="Cost benefits" ID="ID_1862133888" CREATED="1500395236176" MODIFIED="1500395241396">
+<node TEXT="Standard based - hence available tools/expertise" ID="ID_1269002032" CREATED="1500395249952" MODIFIED="1500395261972"/>
+</node>
+</node>
+</node>
+<node TEXT="Capabilities of Identity broker" FOLDED="true" POSITION="right" ID="ID_1470288341" CREATED="1500394211241" MODIFIED="1500394218901">
+<edge COLOR="#00007c"/>
+<node TEXT="Protocol agnostic" ID="ID_312511813" CREATED="1500395490696" MODIFIED="1500395500675">
+<node TEXT="Federation" ID="ID_40038542" CREATED="1500395627437" MODIFIED="1500395630019"/>
+<node TEXT="Transport" ID="ID_596537342" CREATED="1500395631109" MODIFIED="1500395633248"/>
+<node TEXT="Authentication" ID="ID_1258146469" CREATED="1500395633901" MODIFIED="1500395638217"/>
+</node>
+<node TEXT="Claim transformation" ID="ID_981744016" CREATED="1500395501696" MODIFIED="1500395506547">
+<node TEXT="Should be able to transform tokens provided from any identity provider to something understandable by client" ID="ID_1609925731" CREATED="1500395646791" MODIFIED="1500395699320"/>
+</node>
+<node TEXT="Multi-option/multi-step authentication" ID="ID_1191705704" CREATED="1500395511769" MODIFIED="1500395523700">
+<node TEXT="Support multiple ways for authentication" ID="ID_440597501" CREATED="1500395702872" MODIFIED="1500395738461"/>
+</node>
+<node TEXT="Trust brokering" ID="ID_1592233459" CREATED="1500395524809" MODIFIED="1500395528580"/>
+<node TEXT="Home realm discovery" ID="ID_1410841966" CREATED="1500395530217" MODIFIED="1500395536644"/>
+<node TEXT="Adaptive authentication" ID="ID_1819232059" CREATED="1500395537882" MODIFIED="1500395547818">
+<node TEXT="Based on data available" ID="ID_853813532" CREATED="1500395751427" MODIFIED="1500395764125"/>
+</node>
+<node TEXT="Account association" ID="ID_1159238768" CREATED="1500395774619" MODIFIED="1500395782302"/>
+<node TEXT="Multiple attribute stores" ID="ID_390655942" CREATED="1500395782851" MODIFIED="1500395788254"/>
+<node TEXT="Just in time provisioning" ID="ID_567446057" CREATED="1500395788723" MODIFIED="1500395794976"/>
+<node TEXT="Manage identity relationships" ID="ID_516593692" CREATED="1500395795955" MODIFIED="1500395802896">
+<node TEXT="RBAC" ID="ID_443468813" CREATED="1500395837703" MODIFIED="1500395840761"/>
+</node>
+<node TEXT="Centralized access control" ID="ID_1284076920" CREATED="1500395803932" MODIFIED="1500395811591">
+<node TEXT="RBAC" ID="ID_1739094152" CREATED="1500395843013" MODIFIED="1500395858785"/>
+</node>
+<node TEXT="Centralized monitoring &amp; analytics" ID="ID_1891002335" CREATED="1500395813109" MODIFIED="1500395822752">
+<node TEXT="SEIM support" ID="ID_568789169" CREATED="1500395873920" MODIFIED="1500395881522"/>
+</node>
+</node>
+<node TEXT="Federation Problems &amp; Patterns" POSITION="right" ID="ID_1152373980" CREATED="1500394219634" MODIFIED="1500395968313">
+<edge COLOR="#007c00"/>
+<node TEXT="Single Identity across heterogeneous SPs" FOLDED="true" ID="ID_1859081525" CREATED="1500395977069" MODIFIED="1500407661147">
+<node TEXT="Basic Identity federation" ID="ID_1911825625" CREATED="1500396026894" MODIFIED="1500396164390">
+<node TEXT="Pros" ID="ID_408958100" CREATED="1500396055481" MODIFIED="1500396058064">
+<node TEXT="SSO" ID="ID_605820065" CREATED="1500396060727" MODIFIED="1500396062416"/>
+<node TEXT="Separate user auth from app" ID="ID_675965266" CREATED="1500396065048" MODIFIED="1500396138044"/>
+<node TEXT="Hides user credentials from app" ID="ID_746083417" CREATED="1500396075087" MODIFIED="1500396144117"/>
+<node TEXT="Removes administrative overhead from apps" ID="ID_84704760" CREATED="1500396085944" MODIFIED="1500396100796"/>
+<node TEXT="Improves UX" ID="ID_1641406956" CREATED="1500396102040" MODIFIED="1500396108076"/>
+</node>
+<node TEXT="Cons" ID="ID_1902717741" CREATED="1500396113136" MODIFIED="1500396116067">
+<node TEXT="Single point of breach" ID="ID_1619977393" CREATED="1500396118065" MODIFIED="1500396126589"/>
+</node>
+</node>
+</node>
+<node TEXT="Consuming multple services across different trust domains" ID="ID_845483178" CREATED="1500396180860" MODIFIED="1500396243709"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Accessing service providers which reside in different trust domains
+    </p>
+    <p>
+      The interaction may need to be done
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Inter-domain Token Exchange" ID="ID_704470211" CREATED="1500396249744" MODIFIED="1500396260825">
+<node TEXT="Establish a trust relationship between 2 Identity provider" ID="ID_726598422" CREATED="1500396287073" MODIFIED="1500396321029">
+<node TEXT="Identity Federation Patterns (WSO2)_7785492035454670235.png" ID="ID_261875478" CREATED="1500396327250" MODIFIED="1502998936102">
+<hook URI="Identity%20Federation%20Patterns%20(WSO2)_7785492035454670235.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+<node TEXT="SAML" ID="ID_218365939" CREATED="1500396344258" MODIFIED="1500396349084"/>
+<node TEXT="Pros" ID="ID_932896149" CREATED="1500396364386" MODIFIED="1500396367518">
+<node TEXT="Flexible in maintaining trust domains" ID="ID_741707513" CREATED="1500396371731" MODIFIED="1500396383647"/>
+<node TEXT="Facilitates federated interactions between" ID="ID_531156034" CREATED="1500396384891" MODIFIED="1500407790937"/>
+</node>
+</node>
+</node>
+<node TEXT="Intra-domain Token Exchange" ID="ID_1504878821" CREATED="1500396407492" MODIFIED="1500396416279">
+<node TEXT="Interact with a service developed in a federated trust domain without any dependencies to entities in the other trust domain" FOLDED="true" ID="ID_710095632" CREATED="1500396418341" MODIFIED="1500396454002">
+<node TEXT="Pros" ID="ID_141871725" CREATED="1500396482279" MODIFIED="1500396490506">
+<node TEXT="Removes dependencies between consumers and service in different trust domains" ID="ID_1135284812" CREATED="1500396492344" MODIFIED="1500396510915"/>
+<node TEXT="Can handle different token claim representations" ID="ID_1047977847" CREATED="1500396513376" MODIFIED="1500407909621"/>
+</node>
+<node TEXT="Identity Federation Patterns (WSO2)_5791504674690630561.png" ID="ID_1944776414" CREATED="1500396478594" MODIFIED="1500407957133" TEXT_SHORTENED="true">
+<hook URI="Identity%20Federation%20Patterns%20(WSO2)_5791504674690630561.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Identity silos and spaghetti identity" FOLDED="true" ID="ID_1423647590" CREATED="1500396534952" MODIFIED="1500396616420"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Localized grps of svc provicers operating in different protocols with diffculties when it requires interoperability between the service provider grps
+      </li>
+      <li>
+        each service provider has to trust each identity provider
+      </li>
+      <li>
+        not scalable and hard to manage
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Identity Bus" ID="ID_1046896187" CREATED="1500396620926" MODIFIED="1500396626617">
+<node TEXT="Identity Federation Patterns (WSO2)_7367458642001110425.png" ID="ID_151651517" CREATED="1500396690213" MODIFIED="1500396700632" TEXT_SHORTENED="true">
+<hook URI="Identity%20Federation%20Patterns%20(WSO2)_7367458642001110425.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+<node TEXT="Pros" ID="ID_1832592954" CREATED="1500396628165" MODIFIED="1500396630319">
+<node TEXT="Simplicity introducing new trusted domains/svc providers" ID="ID_366614041" CREATED="1500396631350" MODIFIED="1500396641784"/>
+<node TEXT="Loosely coupled" ID="ID_1757240305" CREATED="1500396642245" MODIFIED="1500396647151"/>
+<node TEXT="reduces deployment complexity" ID="ID_303624347" CREATED="1500396647910" MODIFIED="1500396655305"/>
+</node>
+<node TEXT="cons" ID="ID_102276538" CREATED="1500396658590" MODIFIED="1500396660377">
+<node TEXT="Latency" ID="ID_1764501824" CREATED="1500396661719" MODIFIED="1500396665640"/>
+<node TEXT="single point of failure" ID="ID_1363945252" CREATED="1500396666470" MODIFIED="1500396671489"/>
+</node>
+</node>
+</node>
+<node TEXT="Need of dynamic and fine-grained Authorization policies" FOLDED="true" ID="ID_1526305795" CREATED="1500396749506" MODIFIED="1500396818907"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Org policies may require securing svcs beyond typical authorization mechanisms (e.g. beyond RBAC) e.g. based on time or geolocation
+      </li>
+      <li>
+        Service provider needs to define a complex authorization policies to decide whether
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Federated Authorization" ID="ID_1182435569" CREATED="1500396821397" MODIFIED="1500396872365"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Federated authorization
+      </li>
+      <li>
+        XACML can be used to define complex policies
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Identity Federation Patterns (WSO2)_5994355604693437209.png" ID="ID_1080151672" CREATED="1500396876906" MODIFIED="1500396876906">
+<hook URI="Identity%20Federation%20Patterns%20(WSO2)_5994355604693437209.png" SIZE="0.7832898" NAME="ExternalObject"/>
+</node>
+<node TEXT="Pros" ID="ID_1149003369" CREATED="1500396884437" MODIFIED="1500396893104">
+<node TEXT="Authorization decoupled from app" ID="ID_541346912" CREATED="1500396894342" MODIFIED="1500396906038"/>
+<node TEXT="Supports complex authorization" ID="ID_1132882769" CREATED="1500396907861" MODIFIED="1500396916175"/>
+<node TEXT="Avoid duplication across apps" ID="ID_1105046145" CREATED="1500396917029" MODIFIED="1500396926416"/>
+</node>
+<node TEXT="Cons" ID="ID_1220477868" CREATED="1500396933981" MODIFIED="1500396936000">
+<node TEXT="Not widely adapted" ID="ID_64933478" CREATED="1500396937342" MODIFIED="1500396957286"/>
+</node>
+</node>
+</node>
+<node TEXT="Lack of support for federated authorization" FOLDED="true" ID="ID_679174602" CREATED="1500396964107" MODIFIED="1500397017383"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Even if the authentication is federated, most systems does not support authorization in a federated manner. Hence, the SP requires to persists some identity
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Federated unidirectional provisioning" ID="ID_1469309338" CREATED="1500397022158" MODIFIED="1500397084927"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        User interaction is directly with the identity provider
+      </li>
+      <li>
+        IdP initiates the outbound provisioning for service providers
+      </li>
+      <li>
+        SP receives bare minimum amount of information
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Federated bidirectional provisioning" ID="ID_1500567776" CREATED="1500397127650" MODIFIED="1500397202211"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        User provisioning can happen either from IdP or from SP
+      </li>
+      <li>
+        Built on top of unidirectional provisioning
+      </li>
+      <li>
+        User can interact directly with either SP or IdP
+      </li>
+      <li>
+        SP or IdP initiates outbound provisioning
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
