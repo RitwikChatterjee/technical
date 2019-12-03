5750b27a3cf837932ec8c00d80650eec6dd645d3
Ritwik <ritwik.t.chatterjee@gmail.com>
2019-12-03 12:22:08 -0500

    Added negotiating skills

---
+++ Finance/Investing/Notes/Negotiating Skills.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Negotiating Skills" FOLDED="false" ID="ID_233319642" CREATED="1575312094001" MODIFIED="1575312194206" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Chris Voss" POSITION="right" ID="ID_297969523" CREATED="1575312196018" MODIFIED="1575312218842">
+<edge COLOR="#ff0000"/>
+<node TEXT="Establishing rapport" ID="ID_1877440276" CREATED="1575312221089" MODIFIED="1575312227554">
+<node TEXT="Expect surprises, assume nothing" ID="ID_412664642" CREATED="1575323553128" MODIFIED="1575323570033">
+<node TEXT="Assumptions vs hypothesis" FOLDED="true" ID="ID_735541101" CREATED="1575312228450" MODIFIED="1575313002202">
+<node TEXT="Do not assume anything, go with a clean slate" ID="ID_621953025" CREATED="1575313003849" MODIFIED="1575313022282"/>
+<node TEXT="Have a bunch of hypothesis, and test your hypothesis with the information you receive" ID="ID_931413891" CREATED="1575313022681" MODIFIED="1575313042634"/>
+<node TEXT="Question assumptions. Be emotionally open to all possibilities" ID="ID_905156262" CREATED="1575313088985" MODIFIED="1575313149100"/>
+</node>
+</node>
+<node TEXT="Start with &quot;I&apos;m sorry...&quot;" ID="ID_1821176009" CREATED="1575324075089" MODIFIED="1575324086613"/>
+<node TEXT="Mirror" ID="ID_1142142043" CREATED="1575323573951" MODIFIED="1575323576089">
+<node TEXT="Repeat the last 3 words (1-3 critical words)" ID="ID_829218468" CREATED="1575323647104" MODIFIED="1575323663820"/>
+</node>
+<node TEXT="Silence, to let the mirror work" ID="ID_1251017741" CREATED="1575324088600" MODIFIED="1575324098098"/>
+<node TEXT="Repeat" ID="ID_1358380218" CREATED="1575324098969" MODIFIED="1575324101059"/>
+</node>
+<node TEXT="Label their feelings" ID="ID_642506375" CREATED="1575324564777" MODIFIED="1575324585348">
+<node TEXT="Understand and verbalize the predictable emotions of the state" FOLDED="true" ID="ID_260517536" CREATED="1575324586105" MODIFIED="1575324624340">
+<node TEXT="Spot their feelings" ID="ID_1122071507" CREATED="1575324668123" MODIFIED="1575324674517"/>
+<node TEXT="Label/verbalize them" ID="ID_1512720732" CREATED="1575324675321" MODIFIED="1575324680810"/>
+</node>
+<node TEXT="Calmly and respectfully repeat their emotions back to them" FOLDED="true" ID="ID_331278905" CREATED="1575324626057" MODIFIED="1575324664683">
+<node TEXT="Start with" FOLDED="true" ID="ID_1577255056" CREATED="1575324879793" MODIFIED="1575324883603">
+<node TEXT="It seems like..." ID="ID_1583341627" CREATED="1575324883977" MODIFIED="1575324888962"/>
+<node TEXT="It looks like..." ID="ID_956089634" CREATED="1575324889641" MODIFIED="1575324893613"/>
+<node TEXT="It feels like...." ID="ID_1301567896" CREATED="1575324896816" MODIFIED="1575324901563"/>
+</node>
+</node>
+<node TEXT="Silence" ID="ID_1922429756" CREATED="1575324982601" MODIFIED="1575324985771">
+<node TEXT="Don&apos;t follow up with question" ID="ID_1669697784" CREATED="1575324988722" MODIFIED="1575324994899"/>
+<node TEXT="Give it a few seconds for the label to work" ID="ID_95339884" CREATED="1575324995922" MODIFIED="1575325010781"/>
+</node>
+</node>
+<node TEXT="Apologize upfront for your silly mistakes" ID="ID_1626553155" CREATED="1575325887242" MODIFIED="1575325898251">
+<node TEXT="If you have made a silly mistake - apologize upfront" FOLDED="true" ID="ID_643099336" CREATED="1575325899162" MODIFIED="1575325940702">
+<node TEXT="Acknowledge the negative and diffuse it" ID="ID_1426442918" CREATED="1575325941948" MODIFIED="1575325953094"/>
+<node TEXT="Start with" FOLDED="true" ID="ID_111272505" CREATED="1575325977602" MODIFIED="1575326022276">
+<node TEXT="&quot;Look, I&apos;m an idiot....&quot;" ID="ID_360109967" CREATED="1575326023114" MODIFIED="1575326046143"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
