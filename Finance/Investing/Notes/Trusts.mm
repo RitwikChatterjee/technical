40b335215cb7291eab94aeedf5f0518d19378549
Ritwik <bhalo.basha.phx@gmail.com>
2022-02-13 10:11:42 -0700

    RE notes

---
+++ Finance/Investing/Notes/Trusts.mm
@@ -0,0 +1,153 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Trusts" FOLDED="false" ID="ID_835030992" CREATED="1609346299023" MODIFIED="1609346305797" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="What?" FOLDED="true" POSITION="right" ID="ID_341987147" CREATED="1609346308700" MODIFIED="1609346311054">
+<edge COLOR="#ff0000"/>
+<node TEXT="Stakeholders" ID="ID_1186609953" CREATED="1609346320611" MODIFIED="1609346323977">
+<node TEXT="Grantor" ID="ID_451530052" CREATED="1609346324900" MODIFIED="1609346328970"/>
+<node TEXT="Beneficiary" ID="ID_66374693" CREATED="1609346329465" MODIFIED="1609346333499">
+<node TEXT="Can be" FOLDED="true" ID="ID_994162885" CREATED="1609347100157" MODIFIED="1609347105636">
+<node TEXT="Individual" ID="ID_42492906" CREATED="1609347115628" MODIFIED="1609347119597"/>
+<node TEXT="LLC, Series LLC, LP" ID="ID_890327626" CREATED="1609347120005" MODIFIED="1609347134526"/>
+<node TEXT="another trust" ID="ID_1872174183" CREATED="1609347147324" MODIFIED="1609347152820"/>
+</node>
+<node TEXT="Can be changed by grantor using &quot;Assignment of beneficial interest&quot; form" ID="ID_1678785043" CREATED="1609347197606" MODIFIED="1609347941346"/>
+</node>
+<node TEXT="Trustee" ID="ID_78645025" CREATED="1609346333963" MODIFIED="1609346336862"/>
+</node>
+<node TEXT="Characteristics" FOLDED="true" ID="ID_387548954" CREATED="1609346487156" MODIFIED="1609346491510">
+<node TEXT="Cannot own anything without a trustee" ID="ID_1175293418" CREATED="1609346492173" MODIFIED="1609346503036"/>
+</node>
+<node TEXT="Types" ID="ID_517734100" CREATED="1609346352881" MODIFIED="1609346357360">
+<node TEXT="Land" ID="ID_1904568281" CREATED="1609346358354" MODIFIED="1609346361176">
+<node TEXT="Revocable, living trust" ID="ID_144443666" CREATED="1609346886455" MODIFIED="1609346894544"/>
+</node>
+<node TEXT="Living" ID="ID_1777268681" CREATED="1609346361962" MODIFIED="1609346363770"/>
+<node TEXT="Irrevocable Life Insurance" ID="ID_961609420" CREATED="1609346364235" MODIFIED="1609346372910"/>
+</node>
+<node TEXT="Types" ID="ID_1203277678" CREATED="1609346415474" MODIFIED="1609346418066">
+<node TEXT="Revocable" ID="ID_1134287873" CREATED="1609346418757" MODIFIED="1609346424491">
+<node TEXT="Alterable, changeable, terminable at directions of Grantor" ID="ID_1136897920" CREATED="1609346919977" MODIFIED="1609346946400"/>
+</node>
+<node TEXT="Irrevocable" ID="ID_1296981873" CREATED="1609346425116" MODIFIED="1609346430600"/>
+</node>
+</node>
+<node TEXT="Created by" FOLDED="true" POSITION="right" ID="ID_1893724635" CREATED="1609346985818" MODIFIED="1609346992219">
+<edge COLOR="#00ffff"/>
+<node TEXT="a trust agreement" FOLDED="true" ID="ID_126121578" CREATED="1609346999594" MODIFIED="1609347004591">
+<node TEXT="There is no paper trail in the public (anonymity)" ID="ID_1116499259" CREATED="1609346794955" MODIFIED="1609347058359"/>
+<node TEXT="Between grantor &amp; trustee" ID="ID_602250136" CREATED="1609348207372" MODIFIED="1609348216964"/>
+</node>
+<node TEXT="Property transferred to the trust through a deed" FOLDED="true" ID="ID_1269720566" CREATED="1609348253135" MODIFIED="1609348338798">
+<node TEXT="getting the deed recorded at the county office" FOLDED="true" ID="ID_848320292" CREATED="1609347976247" MODIFIED="1609348458728">
+<node TEXT="Has" FOLDED="true" ID="ID_564317900" CREATED="1609347992023" MODIFIED="1609347998531">
+<node TEXT="Grantor" ID="ID_1272297221" CREATED="1609347999554" MODIFIED="1609348003507"/>
+<node TEXT="Trustee" ID="ID_1366057494" CREATED="1609348003930" MODIFIED="1609348006352"/>
+</node>
+<node TEXT="Does not have" FOLDED="true" ID="ID_1254493040" CREATED="1609348010135" MODIFIED="1609348014901">
+<node TEXT="The original trust agreement" ID="ID_1061345447" CREATED="1609348461909" MODIFIED="1609348474748"/>
+<node TEXT="Beneficiary information" ID="ID_99308253" CREATED="1609348015643" MODIFIED="1609348022763"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Why?" FOLDED="true" POSITION="right" ID="ID_1829384963" CREATED="1609346675840" MODIFIED="1609346679109">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Getting your name off a property (or multiple properties)" ID="ID_1467501816" CREATED="1609346680088" MODIFIED="1609346829476"/>
+<node TEXT="Minimizes risks" ID="ID_122213365" CREATED="1609346753989" MODIFIED="1609346758733"/>
+</node>
+<node TEXT="Tax implications" FOLDED="true" POSITION="right" ID="ID_1127818076" CREATED="1609347254480" MODIFIED="1609347258573">
+<edge COLOR="#00007c"/>
+<node TEXT="Does not have EIN" ID="ID_1151760152" CREATED="1609347259648" MODIFIED="1609347267446"/>
+<node TEXT="Does not need to file taxes" ID="ID_1224859216" CREATED="1609347267709" MODIFIED="1609347272899"/>
+<node TEXT="Beneficiary is the tax payer" ID="ID_1860400426" CREATED="1609347284957" MODIFIED="1609347315247"/>
+<node TEXT="No change in tax reporting" ID="ID_794697915" CREATED="1609347327047" MODIFIED="1609347333024"/>
+</node>
+<node TEXT="Arizona requires &quot;Beneficiary&quot; to be disclosed" POSITION="left" ID="ID_1785776252" CREATED="1609346507566" MODIFIED="1609346588130">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Tip" FOLDED="true" POSITION="left" ID="ID_820514509" CREATED="1609348104652" MODIFIED="1609348107074">
+<edge COLOR="#007c00"/>
+<node TEXT="Never serve as your own trustee" FOLDED="true" ID="ID_76661441" CREATED="1609348831191" MODIFIED="1609348846115">
+<node TEXT="Use a trustee with a different last name and state than beneficiary" ID="ID_1727315432" CREATED="1609348108155" MODIFIED="1609348130557"/>
+<node TEXT="Merger Doctrine" ID="ID_1052482696" CREATED="1609348860858" MODIFIED="1609348871817">
+<node TEXT="If &quot;grantor&quot; and &quot;trustee&quot; same person, then trust is not valid" ID="ID_177136070" CREATED="1609348875834" MODIFIED="1609348897894"/>
+</node>
+</node>
+<node TEXT="Using home address on tax statements" ID="ID_369610657" CREATED="1609348899645" MODIFIED="1609348912199"/>
+<node TEXT="Assign interest to LLC" FOLDED="true" ID="ID_583449213" CREATED="1609349069353" MODIFIED="1609349275298">
+<node TEXT="LLC" ID="ID_1610669145" CREATED="1609349083218" MODIFIED="1609349090445"/>
+</node>
+<node TEXT="Do NOT Inform your lender" FOLDED="true" ID="ID_1192144325" CREATED="1609349106554" MODIFIED="1609349202470">
+<node TEXT="Transferring to land trust does not trigger" ID="ID_631521317" CREATED="1609349121661" MODIFIED="1609349181907"/>
+</node>
+<node TEXT="DO NOT refer your trust as a Land Trust" FOLDED="true" ID="ID_1520967286" CREATED="1609349203741" MODIFIED="1609349224733">
+<node TEXT="Call it &quot;Grantor&quot; trust" ID="ID_1090635503" CREATED="1609349310897" MODIFIED="1609349317513"/>
+<node TEXT="Exception states" ID="ID_981572643" CREATED="1609349232415" MODIFIED="1609349240606"/>
+</node>
+</node>
+</node>
+</map>
