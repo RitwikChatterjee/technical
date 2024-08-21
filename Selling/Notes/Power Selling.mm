f09ab6052fa21d3b267acfc7b990aaf8dd41771d
Ritwik <ritwik.t.chatterjee@gmail.com>
2024-08-21 14:33:22 -0500

    Selling course

---
+++ Selling/Notes/Power Selling.mm
@@ -0,0 +1,142 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Power Selling" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1704484294761" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Client Acquisition - Why Change?" POSITION="right" ID="ID_1215224867" CREATED="1704484328292" MODIFIED="1704484345525">
+<edge COLOR="#ff0000"/>
+<node TEXT="Acquisition != Expansion" ID="ID_430215649" CREATED="1704484357492" MODIFIED="1704484392758">
+<node ID="ID_1055542930" CREATED="1704484584430" MODIFIED="1704484594647"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Difference is because of <b><i>Status Quo Bias</i></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="What?" FOLDED="true" ID="ID_1451336981" CREATED="1704484425923" MODIFIED="1704484600151">
+<node TEXT="What someone is already doing today always have a distinct advantage over what we think they should do tomorrow" ID="ID_1230924938" CREATED="1704484499093" MODIFIED="1704484538410"/>
+<node TEXT="60% of closed deals, client chooses to continue doing what they do today" ID="ID_41109640" CREATED="1704484539217" MODIFIED="1704484564721"/>
+</node>
+<node TEXT="Difference" ID="ID_55700791" CREATED="1704484606340" MODIFIED="1704484612421">
+<node TEXT="Acquisition" FOLDED="true" ID="ID_1636540825" CREATED="1704484618963" MODIFIED="1704484639691">
+<node TEXT="You are NOT the status quo" ID="ID_1342714228" CREATED="1704484641833" MODIFIED="1704484648745"/>
+<node TEXT="Your job is to DISRUPT status quo" ID="ID_485899127" CREATED="1704484649280" MODIFIED="1704484663185">
+<node TEXT="WHY CHANGE?" ID="ID_756698407" CREATED="1704484769174" MODIFIED="1704484773896"/>
+</node>
+</node>
+<node TEXT="Expansion" FOLDED="true" ID="ID_1412235625" CREATED="1704484665783" MODIFIED="1704484669296">
+<node TEXT="You ARE the status quo" ID="ID_285004513" CREATED="1704484672694" MODIFIED="1704484678392"/>
+<node TEXT="Your job is to REINFORCE status quo" ID="ID_424704586" CREATED="1704484679471" MODIFIED="1704484691678">
+<node TEXT="WHY STAY?" ID="ID_1173038187" CREATED="1704484777327" MODIFIED="1704484781960"/>
+</node>
+</node>
+<node TEXT="Evolution" FOLDED="true" ID="ID_192800514" CREATED="1704484693805" MODIFIED="1704484716863">
+<node TEXT="You ARE the status quo, but it is not good enough" ID="ID_250329018" CREATED="1704484721302" MODIFIED="1704484733048"/>
+<node TEXT="Your job is to EVOLVE status quo" FOLDED="true" ID="ID_1804765415" CREATED="1704484733622" MODIFIED="1704484740751">
+<node TEXT="WHY EVOLVE?" ID="ID_547394959" CREATED="1704484785166" MODIFIED="1704484791360"/>
+</node>
+</node>
+</node>
+<node TEXT="Causes" ID="ID_398633348" CREATED="1704484928810" MODIFIED="1704484931460">
+<node TEXT="Preference Stability" FOLDED="true" ID="ID_1125167596" CREATED="1704484932187" MODIFIED="1704484937099">
+<node TEXT="What ever choice you have already made always has an advantage over any other choice you might make in the future" ID="ID_1163611931" CREATED="1704484937666" MODIFIED="1704484965955"/>
+</node>
+<node TEXT="Anticipated Blame and Regret" FOLDED="true" ID="ID_1553081748" CREATED="1704485013076" MODIFIED="1704485025405">
+<node TEXT="Mind fast forwards to anticipate all the bad things that can happen when you make the change" ID="ID_285643050" CREATED="1704485038468" MODIFIED="1704485064879"/>
+</node>
+<node TEXT="Perceived Cost of Change" FOLDED="true" ID="ID_1546823264" CREATED="1704485129894" MODIFIED="1704485137137">
+<node TEXT="Many folks think Status Quo is easy. If they change it will take a lot of money, time &amp; energy" ID="ID_454930772" CREATED="1704485138599" MODIFIED="1704485158801"/>
+</node>
+<node TEXT="Selection Difficulty" FOLDED="true" ID="ID_210583066" CREATED="1704485161568" MODIFIED="1704485171745">
+<node TEXT="Even if someone is trying to overcome status quo, if they are offered an overwhelming set of options, the person will probably fallback to status quo" ID="ID_159179917" CREATED="1704485172968" MODIFIED="1704485228556"/>
+</node>
+</node>
+</node>
+<node TEXT="Change is not easy" ID="ID_531068106" CREATED="1704484873984" MODIFIED="1704484879706">
+<node TEXT="Conversations with clients" ID="ID_1755959894" CREATED="1704484906026" MODIFIED="1704484911356">
+<node TEXT="Ask yourself" FOLDED="true" ID="ID_213205890" CREATED="1704484824448" MODIFIED="1704484829985">
+<node TEXT="how much of the status quo do I play a part in?" ID="ID_298196465" CREATED="1704484831840" MODIFIED="1704484840977"/>
+<node TEXT="Which elements of the status quo bias do I need to reinforce/disrupt?" ID="ID_1594388454" CREATED="1704485241433" MODIFIED="1704485273252"/>
+</node>
+<node TEXT="To disrupt" FOLDED="true" ID="ID_356447318" CREATED="1704485708231" MODIFIED="1704485714641">
+<node TEXT="Make status quo seem like more of a risk than changing" ID="ID_1703426643" CREATED="1704485717703" MODIFIED="1704485730688"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
