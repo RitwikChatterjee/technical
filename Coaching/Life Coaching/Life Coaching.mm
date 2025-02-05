5cb7607c26466996c854687feca0fa3d6f7cf8f5
Ritwik <bhalo.basha.phx@gmail.com>
2025-02-05 16:11:39 -0700

    Life Coaching, estate planning, llc and trusts

---
+++ Coaching/Life Coaching/Life Coaching.mm
@@ -0,0 +1,190 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Life Coaching" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1653856126150" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Preparing for a session" POSITION="right" ID="ID_1537611656" CREATED="1653856128113" MODIFIED="1653856137762">
+<edge COLOR="#ff0000"/>
+<node TEXT="Pre-thoughts" FOLDED="true" ID="ID_609117618" CREATED="1653856139474" MODIFIED="1653856159460">
+<node TEXT="The nervousness should be channeled in a positive way" ID="ID_1156724728" CREATED="1653856160354" MODIFIED="1653856173822"/>
+</node>
+<node TEXT="Prepare yourself" FOLDED="true" ID="ID_220467136" CREATED="1653856435543" MODIFIED="1653856440974">
+<node TEXT="Regulate your emotional state" FOLDED="true" ID="ID_360249794" CREATED="1653856235428" MODIFIED="1653856313245">
+<node TEXT="Why?" ID="ID_43163330" CREATED="1653856452842" MODIFIED="1653856456312">
+<node TEXT="Emotions may run high - and we can start reflect in your behaviour" ID="ID_926357001" CREATED="1653856279501" MODIFIED="1653856305970"/>
+</node>
+<node TEXT="How?" FOLDED="true" ID="ID_517570820" CREATED="1653856242879" MODIFIED="1653856244636">
+<node TEXT="Meditate/listen to music to calm your nervous system" ID="ID_1722111442" CREATED="1653856315560" MODIFIED="1653856340626"/>
+<node TEXT="Go out in nature to calm your nervous system" ID="ID_973162719" CREATED="1653856359880" MODIFIED="1653856373432"/>
+</node>
+</node>
+<node TEXT="Housekeeping" FOLDED="true" ID="ID_75696043" CREATED="1653856656233" MODIFIED="1653856671400">
+<node TEXT="Take a bio-break before the coaching session" ID="ID_1928488651" CREATED="1653856672577" MODIFIED="1653856690274"/>
+<node TEXT="Keep a bottle of water handy" ID="ID_866716199" CREATED="1653856709160" MODIFIED="1653856720326"/>
+</node>
+<node TEXT="Prep" ID="ID_692962545" CREATED="1653856863845" MODIFIED="1653856875066">
+<node TEXT="Focus on your why?" ID="ID_1014918782" CREATED="1653856807307" MODIFIED="1653856817043">
+<node TEXT="Remind yourself of your purpose" ID="ID_819686555" CREATED="1653856822921" MODIFIED="1653856830423">
+<node TEXT="This will give you the power/energy" ID="ID_1331382559" CREATED="1653856831346" MODIFIED="1653856849029"/>
+</node>
+</node>
+<node TEXT="It is about them" ID="ID_1218871184" CREATED="1653856854596" MODIFIED="1653856860617">
+<node TEXT="Take your ego out of the way" ID="ID_1725821000" CREATED="1653856889543" MODIFIED="1653856897285"/>
+<node TEXT="Your coachee is coming to the session to get an outcome" ID="ID_485862932" CREATED="1653856930500" MODIFIED="1653856948586"/>
+<node TEXT="You don&apos;t have to be" FOLDED="true" ID="ID_336072733" CREATED="1653856897863" MODIFIED="1653856968278">
+<node TEXT="100% right" ID="ID_3242924" CREATED="1653856970127" MODIFIED="1653856976058"/>
+<node TEXT="a Guru" ID="ID_1009276730" CREATED="1653856976550" MODIFIED="1653856980104"/>
+<node TEXT="know-it-all" ID="ID_1932321849" CREATED="1653857034585" MODIFIED="1653857038728"/>
+</node>
+<node TEXT="You have to be" FOLDED="true" ID="ID_1569109642" CREATED="1653856981322" MODIFIED="1653856985950">
+<node TEXT="someone curious to bring an outcome for your client" ID="ID_868606564" CREATED="1653856987265" MODIFIED="1653857003407"/>
+</node>
+<node TEXT="Top priority" FOLDED="true" ID="ID_1007196704" CREATED="1653857020466" MODIFIED="1653857025001">
+<node TEXT="Create an outcome for them" ID="ID_1789556788" CREATED="1653857026456" MODIFIED="1653857032413"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Structuring the session" FOLDED="true" ID="ID_1409428278" CREATED="1653857162747" MODIFIED="1653857168502">
+<node TEXT="Share a &quot;win&quot; (5 mins)" FOLDED="true" ID="ID_797242271" CREATED="1653857195694" MODIFIED="1653857288935">
+<node TEXT="Ask for a win from the client" ID="ID_879453004" CREATED="1653857213204" MODIFIED="1653857218668"/>
+<node TEXT="Helps boosting confidence and start on a positive note" ID="ID_1293695085" CREATED="1653857219133" MODIFIED="1653857246382"/>
+</node>
+<node TEXT="Checking in with previous commitments (5 mins)" FOLDED="true" ID="ID_243752144" CREATED="1653857289569" MODIFIED="1653857301982">
+<node TEXT="Check with what happened" FOLDED="true" ID="ID_1078559293" CREATED="1653857358195" MODIFIED="1653857368679">
+<node TEXT="If not achieved, try to find the cause of what got in the way" FOLDED="true" ID="ID_324251271" CREATED="1653857369515" MODIFIED="1653857382618">
+<node TEXT="Ask open ended questions" ID="ID_1222121393" CREATED="1653857400540" MODIFIED="1653857407106"/>
+</node>
+<node TEXT="If achieved, ask what went well, what did not go so well" ID="ID_740017677" CREATED="1653857457278" MODIFIED="1653857474092"/>
+</node>
+<node TEXT="DON&apos;T get judgemental or criticize" FOLDED="true" ID="ID_492314606" CREATED="1653857522630" MODIFIED="1653857563453">
+<node TEXT="Try to find the limiting beliefs instead" ID="ID_1257153871" CREATED="1653857582007" MODIFIED="1653857604794"/>
+</node>
+</node>
+<node TEXT="Ask for a coaching request/objective" FOLDED="true" ID="ID_255410932" CREATED="1653857617239" MODIFIED="1653857629490">
+<node TEXT="Very important as if objective is not established, it cannot be measured" ID="ID_1571281183" CREATED="1653857634506" MODIFIED="1653857653288"/>
+<node TEXT="E.g." FOLDED="true" ID="ID_1006058975" CREATED="1653857669275" MODIFIED="1653857671808">
+<node TEXT="What do you want to focus on today" ID="ID_774893741" CREATED="1653857672667" MODIFIED="1653857687212"/>
+</node>
+<node TEXT="Co-create instead of &quot;telling&quot; them" ID="ID_839343866" CREATED="1653857721300" MODIFIED="1653857731753"/>
+</node>
+<node TEXT="Actual coaching session" FOLDED="true" ID="ID_1334756559" CREATED="1653857767806" MODIFIED="1653857774968">
+<node TEXT="Skills" FOLDED="true" ID="ID_1844508514" CREATED="1653857776094" MODIFIED="1653857780040">
+<node TEXT="Active listening" ID="ID_839724648" CREATED="1653857781070" MODIFIED="1653857788400"/>
+<node TEXT="Powerful questioning" ID="ID_484977448" CREATED="1653857788910" MODIFIED="1653857795075"/>
+<node TEXT="Mirroring" ID="ID_838594171" CREATED="1653857796061" MODIFIED="1653857799530"/>
+<node TEXT="Paraphrasing" ID="ID_1141993996" CREATED="1653857815191" MODIFIED="1653857819071"/>
+<node TEXT="Reflective inquiry" ID="ID_691301966" CREATED="1653857825967" MODIFIED="1653857831102"/>
+</node>
+<node TEXT="Life advice" FOLDED="true" ID="ID_1808386358" CREATED="1653857877504" MODIFIED="1653857885645">
+<node TEXT="Normally don&apos;t" ID="ID_816879032" CREATED="1653857887328" MODIFIED="1653857893014"/>
+<node TEXT="If you do" FOLDED="true" ID="ID_264898927" CREATED="1653857893568" MODIFIED="1653857897437">
+<node TEXT="Like self/other client story" ID="ID_1278568024" CREATED="1653857898417" MODIFIED="1653857907655"/>
+<node TEXT="Ask for permission first" ID="ID_1338189806" CREATED="1653857911433" MODIFIED="1653857934351"/>
+</node>
+</node>
+</node>
+<node TEXT="Summarize (last 10 mins)" FOLDED="true" ID="ID_1088107573" CREATED="1653858075878" MODIFIED="1653858090811">
+<node TEXT="Ask if they are satisfied with the session" ID="ID_1351807914" CREATED="1653858105030" MODIFIED="1653858146861"/>
+<node TEXT="Co-create SMART goals" FOLDED="true" ID="ID_412093624" CREATED="1653858147375" MODIFIED="1653858154869">
+<node TEXT="Don&apos;t prescribe" ID="ID_1803538847" CREATED="1653858168044" MODIFIED="1653858173792"/>
+<node TEXT="Ask them for their ideas/solutions" ID="ID_1229557173" CREATED="1653858174366" MODIFIED="1653858189031"/>
+<node TEXT="Put a deadline and make it more measurable" ID="ID_1717760003" CREATED="1653858207694" MODIFIED="1653858223971"/>
+</node>
+<node TEXT="Add accountability" FOLDED="true" ID="ID_744378021" CREATED="1653858229040" MODIFIED="1653858233058">
+<node TEXT="Ask them how would they measure success?" ID="ID_794701004" CREATED="1653858233703" MODIFIED="1653858254064"/>
+<node TEXT="How would they communicate with you/be accountable" FOLDED="true" ID="ID_1169108638" CREATED="1653858254450" MODIFIED="1653858265929">
+<node TEXT="Send you an update" ID="ID_1410782360" CREATED="1653858267777" MODIFIED="1653858271547"/>
+<node TEXT="You should not be the one calling on them, they should hold themselves accountable" ID="ID_1918871376" CREATED="1653858287186" MODIFIED="1653858308771"/>
+</node>
+<node TEXT="Can use friends and family for accountability" ID="ID_461278549" CREATED="1653858348442" MODIFIED="1653858375467"/>
+</node>
+<node TEXT="Wrap up" FOLDED="true" ID="ID_434572646" CREATED="1653858376667" MODIFIED="1653858405288">
+<node TEXT="Closeout questions" FOLDED="true" ID="ID_477326970" CREATED="1653858409287" MODIFIED="1653858413228">
+<node TEXT="What did you find most useful in today&apos;s session" ID="ID_1065692744" CREATED="1653858414141" MODIFIED="1653858424585"/>
+<node TEXT="Where might you have a breakthrough" ID="ID_264256031" CREATED="1653858432328" MODIFIED="1653858447261"/>
+<node TEXT="What did you learn about yourself from today&apos;s session" ID="ID_697723512" CREATED="1653858454733" MODIFIED="1653858466609"/>
+</node>
+</node>
+<node TEXT="Acknowledge" FOLDED="true" ID="ID_218932799" CREATED="1653858486265" MODIFIED="1653858489951">
+<node TEXT="Acknowledge the client taking the time to improve" ID="ID_996165531" CREATED="1653858490768" MODIFIED="1653858504679"/>
+<node TEXT="Help client celebrate themselves" ID="ID_823537386" CREATED="1653858513904" MODIFIED="1653858525249"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="4 Steps for successful coaching session" POSITION="left" ID="ID_1622436426" CREATED="1653856492789" MODIFIED="1653856563541">
+<edge COLOR="#0000ff"/>
+<node TEXT="Build rapport" ID="ID_337955976" CREATED="1653856515138" MODIFIED="1653856519166"/>
+<node TEXT="Define an objective" ID="ID_298943011" CREATED="1653856519509" MODIFIED="1653856527733"/>
+<node TEXT="Provide coaching" ID="ID_617955195" CREATED="1653856528828" MODIFIED="1653856533699"/>
+<node TEXT="Follow up and action steps" ID="ID_176364512" CREATED="1653856541230" MODIFIED="1653856547492"/>
+</node>
+</node>
+</map>
